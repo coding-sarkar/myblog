5d829fc35df0e46b4ef1f73ad32724fe705c83c2
Souvik Dey Sarkar <sdeys2111@gmail.com>
2022-07-04 17:18:58 +0530

    Add wysiwyg editor

--- articles/models.py
+++ articles/models.py
@@ -2,11 +2,12 @@ from statistics import mode
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-
+from ckeditor.fields import RichTextField
 
 class Article(models.Model):
   title = models.CharField(max_length=255)
-  body = models.TextField()
+  # body = models.TextField()
+  body = RichTextField(blank=True, null=True)
   date = models.DateTimeField(auto_now_add=True)
   author = models.ForeignKey(
     get_user_model(),
--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'articles',
 
     'crispy_forms',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
